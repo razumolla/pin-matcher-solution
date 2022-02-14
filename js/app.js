4dd773b01269d9f8ef426b85454f4f23b0c1c4f5
razumolla <rajuahmed9344@gmail.com>
2022-02-14 15:28:02 +0600

    pin matched update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Random pin Generate, class: 28.5
 function getPin() {
     const pin = Math.round(Math.random() * 10000);
     const pinstring = pin + '';
@@ -12,5 +13,43 @@ function getPin() {
 function generatePin() {
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
-    console.log(pin)
+    // console.log(pin)
+}
+
+// 2nd part: class-28.6
+document.getElementById('key-pad').addEventListener('click',
+    function (event) {
+        const calcInput = document.getElementById('typed-numbers');
+        const number = event.target.innerText;
+        if (isNaN(number)) {
+            if (number == 'C') {
+                calcInput.value = '';
+            }
+        }
+        else {
+            const previousNumber = calcInput.value;
+            const newNumber = previousNumber + number
+            calcInput.value = newNumber;
+        }
+
+    }
+);
+
+//  submit button event handler
+
+function verifyPin() {
+    const randomPin = document.getElementById('display-pin').value;
+    const typeNumbers = document.getElementById('typed-numbers').value;
+    const pinSuccess = document.getElementById('notify-success');
+    const pinError = document.getElementById('notify-fail');
+
+    if (randomPin == typeNumbers) {
+        pinSuccess.style.display = 'block';
+        pinError.style.display = 'none'; //error msg hide
+
+    } else {
+        pinError.style.display = 'block';
+        pinSuccess.style.display = 'none';//success msg hide
+
+    }
 }
